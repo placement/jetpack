fa5a526943809730c9223578d2c57fb393f5f7c6
retrofox <rdsuarez@gmail.com>
2020-03-13 14:29:49 -0300

    podcast-player: implement colors setting pane

--- extensions/blocks/podcast-player/edit.js
+++ extensions/blocks/podcast-player/edit.js
@@ -26,7 +26,9 @@ import { __ } from '@wordpress/i18n';
 import {
 	BlockControls,
 	BlockIcon,
+	ContrastChecker,
 	InspectorControls,
+	PanelColorSettings,
 	withColors,
 	__experimentalUseColors as useColors,
 } from '@wordpress/block-editor';
@@ -87,7 +89,18 @@ const FeedURLControl = ( {
 	/>
 );
 
-const PodcastPlayerEdit = ( { attributes, setAttributes } ) => {
+const PodcastPlayerEdit = ( {
+	attributes,
+	setAttributes,
+
+	textColor,
+	setTextColor,
+	fallbackTextColor,
+
+	backgroundColor,
+	setBackgroundColor,
+	fallbackBackgroundColor,
+} ) => {
 	// Block attributes.
 	const { url, itemsToShow, showCoverArt, showEpisodeDescription } = attributes;
 
@@ -191,6 +204,36 @@ const PodcastPlayerEdit = ( { attributes, setAttributes } ) => {
 						onChange={ value => setAttributes( { showEpisodeDescription: value } ) }
 					/>
 				</PanelBody>
+
+				{ ! isUseColorsAvailable && (
+					<PanelColorSettings
+						title={ __( 'Color Settings', 'jetpack' ) }
+						colorSettings={ [
+							{
+								value: textColor.color,
+								onChange: setTextColor,
+								label: __( 'Text Color', 'jetpack' ),
+							},
+							{
+								value: backgroundColor.color,
+								onChange: setBackgroundColor,
+								label: __( 'Background', 'jetpack' ),
+							},
+						] }
+					>
+						<ContrastChecker
+							{ ...{
+								// Text is considered large if font size is greater or equal to 18pt or 24px,
+								// currently that's not the case for button.
+								isLargeText: false,
+								textColor: textColor.color,
+								backgroundColor: backgroundColor.color,
+								fallbackBackgroundColor,
+								fallbackTextColor,
+							} }
+						/>
+					</PanelColorSettings>
+				) }
 			</InspectorControls>
 			<Disabled>
 				<ServerSideRender
