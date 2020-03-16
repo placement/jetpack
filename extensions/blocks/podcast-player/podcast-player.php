567a7277b84f9528cad31f93d09612629fd833f2
retrofox <rdsuarez@gmail.com>
2020-03-16 12:12:43 -0300

    [not verified] podcast-player: add basic ss rendering

--- extensions/blocks/podcast-player/podcast-player.php
+++ extensions/blocks/podcast-player/podcast-player.php
@@ -108,7 +108,13 @@ function render_player( $track_list, $attributes ) {
 
 	?>
 	<div class="<?php echo esc_attr( $block_classname ); ?>">
-		<?php // Placeholder: block markup is being handled in https://github.com/Automattic/jetpack/pull/14952. ?>
+		<ol>
+			<?php
+			foreach ( $track_list as $att_id => $attachment ) :
+				printf( '<li><a href="%1$s" data-jetpack-podcast-audio="%2$s">%3$s</a></li>', esc_url( $attachment['link'] ), esc_url( $attachment['src'] ), esc_html( $attachment['title'] ) );
+			endforeach;
+			?>
+		</ol>
 		<script type="application/json" class="wp-playlist-script"><?php echo wp_json_encode( $player_data ); ?></script>
 	</div>
 	<?php
